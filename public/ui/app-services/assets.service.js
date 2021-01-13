2c1842034f3ef6e69083605e22caf77a4c8948a8
Ohtarr <ohtarr@gmail.com>
2021-01-13 10:28:15 -0600

    Modified/cleaned up services, added some error handling to parts controller

--- public/ui/app-services/assets.service.js
+++ public/ui/app-services/assets.service.js
@@ -6,37 +6,12 @@ angular
 
 		// Get Assets
 		self.getAssets = function(httpParams) {
-/* 			var defer = $q.defer();
-			return $http.get(globalUrl + '/api/assets',{params: httpParams})
-				.then(function successCallback(response) {
-					//console.log(response)
-					defer.resolve(response);
-
-					// Must return the promise to the controller.
-					return defer.promise;
-
-			  }, function errorCallback(response) {
-					defer.resolve(response);
-					return defer.promise;
-			  }); */
 			return $http.get(globalUrl + '/api/assets',{params: httpParams})
 		}
 
 		// Get Asset by ID
 		self.getAsset = function(id) {
-/* 			var defer = $q.defer();
 			return $http.get(globalUrl + '/api/assets/'+id)
-				.then(function successCallback(response) {
-					defer.resolve(response);
-
-					// Must return the promise to the controller.
-					return defer.promise;
-
-			  }, function errorCallback(response) {
-					defer.resolve(response);
-					return defer.promise;
-			  }); */
-			  return $http.get(globalUrl + '/api/assets/'+id)
 		}
 
 		// Create Asset
@@ -46,46 +21,12 @@ angular
 
 		// Update Asset by ID
 		self.updateAsset = function(id, update) {
-
-/* 			return $http.put(globalUrl + '/api/assets/'+id, update).then(
-				function(response) {
-					//return response.data;
-					return response;
-				}, function(error) {
-					//alert('An error occurred while updating the Asset') ;
-					return error;
-				}
-			); */
-
-/* 			$http.put(globalUrl + '/api/assets/'+id, update).then(
-				(data) => {
-					return null, data
-				},
-				(error) => {
-					return error
-				}
-			); */
-/* 			$http.put(globalUrl + '/api/assets/'+id, update)
-				.then(function (response) {
-					return response.data;
-				})
-				.catch(function (data) {
-					// Handle error here
-				}); */
 			return $http.put(globalUrl + '/api/assets/'+id, update);
-
 		}
 
 		// Delete Asset by ID
 		self.deleteAsset = function(id) {
-/* 			console.log('Service - Deleting ID: '+ id);
-			return $http.delete(globalUrl + '/api/assets/'+id, id).then(function(response) {
-
-				var data = response.data;
-				return data;
-
-			 }); */
-			 return $http.delete(globalUrl + '/api/assets/'+id, id)
+			return $http.delete(globalUrl + '/api/assets/'+id, id)
 		}
 
 		return self
--- public/ui/app-services/locations.service.js
+++ public/ui/app-services/locations.service.js
@@ -6,66 +6,13 @@ angular
 
 		// Get Locations
 		self.getLocations = function(httpParams) {
-/* 			console.log("HTTP PARAMS:");
-			console.log(httpParams); */
-			var defer = $q.defer();
 			return $http.get(globalUrl + '/api/locations')
-				.then(function successCallback(response) {
-					//console.log(response)
-					defer.resolve(response);
-
-					// Must return the promise to the controller.
-					return defer.promise;
-
-			  }, function errorCallback(response) {
-					defer.resolve(response);
-					return defer.promise;
-			  });
 		}
 
 		// Get Location by ID
 		self.getLocation = function(id) {
-			var defer = $q.defer();
 			return $http.get(globalUrl + '/api/locations/'+id)
-				.then(function successCallback(response) {
-					defer.resolve(response);
-
-					// Must return the promise to the controller.
-					return defer.promise;
-
-			  }, function errorCallback(response) {
-					defer.resolve(response);
-					return defer.promise;
-			  });
 		}
 
-/* 		// Create Location
-		self.createLocation = function(location) {
-			return $http.post(globalUrl + '/api/locations',location);
-		} */
-
-/* 		// Update Location by ID
-		self.updateLocation = function(id, update) {
-
-			return $http.put(globalUrl + '/api/locations/'+id, update).then(function(response) {
-
-				var data = response.data;
-				return data;
-
-			 }, function(error) {return false;});
-		} */
-
-/* 		// Delete Location by ID
-		self.deleteLocation = function(id) {
-			console.log('Service - Deleting ID: '+ id);
-			return $http.delete(globalUrl + '/api/locations/'+id, id).then(function(response) {
-
-				var data = response.data;
-				return data;
-
-			 });
-		}
-*/
 		return self 
-
 	}]);
